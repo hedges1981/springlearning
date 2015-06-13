b0168ba63f90c2a4caea3b733536cdace1554ace
hedges1981 <christine@christinelaptop>
2015-06-13 12:34:24 +0100

    jjjj

---
+++ spring-mvc-learning/src/main/java/com/hedges/springlearning/mvc/SampleListener.java
@@ -0,0 +1,24 @@
+package com.hedges.springlearning.mvc;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.log4j.Logger;
+
+public class SampleListener implements ServletContextListener
+{
+	private static final Logger LOGGER = Logger.getLogger(SampleListener.class);
+	
+	public void contextInitialized(ServletContextEvent sce) {
+		
+		LOGGER.info("Context intialised");
+		
+	}
+
+	public void contextDestroyed(ServletContextEvent sce) {
+		
+		LOGGER.info("Context destroyed");
+		
+	}
+
+}
