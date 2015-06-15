c428d5aac53ac0b375e4c0d6edb165966ca7c054
rowland-hall <rowlandhall@yahoo.co.uk>
2015-06-15 11:54:12 +0100

    jjjj

---
+++ mockitolearning/src/main/java/AClass.java
@@ -0,0 +1,17 @@
+/**
+ * Created by rowland-hall on 22/01/15
+ */
+public class AClass
+{
+    public void doSomething( AnotherClass anotherClass )
+    {
+        doSomethingElse( anotherClass );
+    }
+
+    public void doSomethingElse( AnotherClass anotherClass )
+    {}
+
+
+
+
+}
---
+++ mockitolearning/src/main/java/AnotherClass.java
@@ -0,0 +1,19 @@
+/**
+ * Created by rowland-hall on 04/06/15
+ */
+public class AnotherClass
+{
+    String text;
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public void setText( String text )
+    {
+
+        this.text = text;
+    }
+}
+
