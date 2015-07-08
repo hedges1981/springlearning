16f790b5004bf47f1c74967d8f3d8bcd984bcdb7
rowland-hall <rowlandhall@yahoo.co.uk>
2015-07-08 09:14:04 +0100

    asasa

--- spring-mvc-security-learning/src/main/java/com/hedges/springlearning/mvc/securitylearning/SecurityLearningController.java
+++ spring-mvc-security-learning/src/main/java/com/hedges/springlearning/mvc/securitylearning/SecurityLearningController.java
@@ -9,18 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Created by rowland-hall on 07/07/15
  *
  * url is localhost:2702/springmvclearning/test/securitylearning/user
- *
- * localhost:2702/springmvclearning/test/securitylearning/admin
+ * localhost:2702/springmvclearning/test/securitylearning/adminOrUser
+ * localhost:2702/springmvclearning/test/securitylearning/admin    localhost:2702/springmvclearning/test/securitylearning/adminAndUser
  */
 @Controller
 @RequestMapping(value="/securitylearning")
 public class SecurityLearningController
 {
-    public SecurityLearningController()
-    {
-        int x=0;
-    }
-
     @RequestMapping(method = RequestMethod.GET)
     public String ok( ModelMap model )
     {
@@ -28,7 +23,6 @@ public class SecurityLearningController
 
         return "printMessage";
     }
-
 
     @RequestMapping(value="/user**", method = RequestMethod.GET)
     public String userRequest( ModelMap model )
@@ -42,6 +36,20 @@ public class SecurityLearningController
     public String adminRequest( ModelMap model )
     {
         model.addAttribute( "message","in admin area");
+        return "printMessage";
+    }
+
+    @RequestMapping(value = "/adminAndUser**", method = RequestMethod.GET)
+    public String adminAndUserRequest( ModelMap model )
+    {
+        model.addAttribute( "message","in admin and User area");
+        return "printMessage";
+    }
+
+    @RequestMapping(value = "/adminOrUser**", method = RequestMethod.GET)
+    public String adminOrUserRequest( ModelMap model )
+    {
+        model.addAttribute( "message","in admin OR User area");
         return "printMessage";
     }
 
