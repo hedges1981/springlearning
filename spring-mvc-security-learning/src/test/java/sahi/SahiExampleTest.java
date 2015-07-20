ae2df28489a1f966f170d1db82254400a1cf474b
rowland-hall <rowlandhall@yahoo.co.uk>
2015-07-20 13:56:27 +0100

    sdsdsd

--- spring-mvc-security-learning/src/test/java/sahi/SahiExampleTest.java
+++ spring-mvc-security-learning/src/test/java/sahi/SahiExampleTest.java
@@ -13,66 +13,21 @@ import java.util.HashMap;
 public class SahiExampleTest
 {
 
-    /**
-     - ensure fireFox installed on browser
-     - follow: https://sahipro.com/docs/using-sahi/configuring-sahi-with-xvbf.html to get xvbf on box including configuring
-     firefox to use it in the sahi browsers xml file.
-
-     Note instructions are crap on there. basically:
-     -download the file.
-     -extract the Xvbf file from the tar.
-     - move it to : /usr/X11R6/bin/Xvfb
-     -chmod 777
-     - create file in usr/bin with contents:
-
-     #!/bin/sh
-     mode=$1
-     case "$mode" in
-     'start')
-     if [ -f /usr/X11R6/bin/Xvfb ]; then
-     echo "<strong></strong><strong>Starting up the Virtual Frame Buffer on Screen 1</strong><strong></strong>"
-     /usr/X11R6/bin/Xvfb :1 -screen 0 1152x900x8 &
-     fi
-     ;;
-     *)
-     echo " Usage: "
-     echo " $0 start (start XVFB)"
-     echo " $0 stop (stop XVFB - not supported)"
-     exit 1
-     ;;
-     esac
-     exit 0
-
-     - to run it call Xvbf start.
-
-     - to get sahi to use the headless firefox, put in browser_types.xml:
-
-     <browserType>
-     <name>firefox-xvfb</name>
-     <displayName>Firefox</displayName>
-     <icon>firefox.png</icon>
-     <path>env DISPLAY=:1 firefox</path>
-     <options>-profile "$userDir/browser/ff/profiles/sahi$threadNo" -no-remote</options>
-     <processName>firefox</processName>
-     <capacity>5</capacity>
-     </browserType>
-
-
-     Use browser type firefox-xvfb
-
-
-     */
     Process sahiProcess;
 
     String sahiBinFolder = "/home/rowland-hall/sahi/bin";
 
     private void startUpSahi()
     {
+        String startUpScriptFolder = System.getProperty( "startUpScriptFolder" );
+
         try
         {
             sahiProcess = Runtime.getRuntime().exec( sahiBinFolder+"/./sahi.sh", null, new File(sahiBinFolder) );
+            //need to sleep a bit here to allow it to start up b4 running the test.
+            Thread.currentThread().sleep(10000);
         }
-        catch ( IOException e )
+        catch ( Exception e )
         {
             throw new RuntimeException( e );
         }
@@ -92,7 +47,7 @@ public class SahiExampleTest
 
             //RUN A TEST:
             String suiteName = "/home/rowland-hall/personal_ws/springlearning-git/spring-mvc-security-learning/src/test/sahi/sahiExampleTest.sah";
-            String browserType = "firefox-xvfb";
+            String browserType = "firefox";
             String base = "http://localhost:2702/springmvclearning/test/sahiexample/page1";
             String threads = "1";
             TestRunner testRunner =
@@ -109,7 +64,7 @@ public class SahiExampleTest
         }
         finally
         {
-            sahiProcess.destroy();
+           sahiProcess.destroy();
         }
     }
 }
