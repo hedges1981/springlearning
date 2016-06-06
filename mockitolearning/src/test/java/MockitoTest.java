80d205686c67d718740ee9dea869d3bc724c46c6
rowland-hall <rowlandhall@yahoo.co.uk>
2016-06-06 09:59:21 +0100

    kk

--- mockitolearning/src/test/java/MockitoTest.java
+++ mockitolearning/src/test/java/MockitoTest.java
@@ -1,5 +1,6 @@
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentMatcher;
+import org.mockito.*;
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -13,33 +14,46 @@ import static org.mockito.Mockito.*;
 
 /**
  * Created by rowland-hall on 22/01/15
- *
- *  Follows mockito user guide:
- *
- *  https://docs.google.com/document/d/15mJ2Qrldx-J14ubTEnBj7nYN2FB8ap7xOn8GRAi24_A/edit?pli=1
- *
+ * <p/>
+ * Follows mockito user guide:
+ * <p/>
+ * https://docs.google.com/document/d/15mJ2Qrldx-J14ubTEnBj7nYN2FB8ap7xOn8GRAi24_A/edit?pli=1
  */
 public class MockitoTest
 {
-    void p(Object o)
+    /**
+     * *NOTE this bit demos standard way of setting up mocks and a test object injected with them:***
+     */
+    @Mock
+    AnotherClass anotherClassMOck;
+    @InjectMocks
+    AClass aClassInjectedWithMocks = new AClass();
+
+    @Before //need this to get the mocks set up. Can also set-up the mocking behaviour in this method.
+    public void initMocks()
     {
-        System.out.println(o);
+        MockitoAnnotations.initMocks( this );
+    }
+
+    void p( Object o )
+    {
+        System.out.println( o );
     }
 
     @Test
     public void _1letsVerifySomeBehaviour()
     {
-        List mockedList = mock(List.class);
+        List mockedList = mock( List.class );
 
         //using mock object
-        mockedList.add("one");
+        mockedList.add( "one" );
         mockedList.clear();
 
         //verifies that mockedList.add("one") was called:
-        verify(mockedList).add("one");
+        verify( mockedList ).add( "one" );
 
         //verifies that mockedList.clear() was called:
-        verify(mockedList).clear();
+        verify( mockedList ).clear();
 
         //this will cause the test to fail, as the method was not called with this param:
         //verify(mockedList).add("gfhghghg");
@@ -47,42 +61,42 @@ public class MockitoTest
         //resets the mock:
         reset( mockedList );
 
-        mockedList.add("one");
-        mockedList.add("one");
+        mockedList.add( "one" );
+        mockedList.add( "one" );
 
         //verifies that it was called 2 times.
-        verify(mockedList, times(2)).add("one");
+        verify( mockedList, times( 2 ) ).add( "one" );
     }
 
     @Test
     public void _2howAboutSomeStubbing()
     {
-        LinkedList mockedList = mock(LinkedList.class) ;
+        LinkedList mockedList = mock( LinkedList.class );
 
         //stubbing: mocking up return values from calls:
-        when(mockedList.get(0)).thenReturn("0");
-        when(mockedList.get(1)).thenThrow( new RuntimeException() );
+        when( mockedList.get( 0 ) ).thenReturn( "0" );
+        when( mockedList.get( 1 ) ).thenThrow( new RuntimeException() );
 
         //will give 0
-        p(mockedList.get(0));
+        p( mockedList.get( 0 ) );
 
         //will throw exception:
         try
         {
-            mockedList.get(1);
+            mockedList.get( 1 );
         }
-        catch(Exception e)
+        catch ( Exception e )
         {
-            p(e);
+            p( e );
         }
 
         //this will give null, as we have not mocked this call:
-        p(mockedList.get(33));
+        p( mockedList.get( 33 ) );
 
 
-        when(mockedList.get(0)).thenReturn("value has been changed");
+        when( mockedList.get( 0 ) ).thenReturn( "value has been changed" );
         //will give the new value:
-        p(mockedList.get(0));
+        p( mockedList.get( 0 ) );
     }
 
     @Test
@@ -91,23 +105,23 @@ public class MockitoTest
         LinkedList mockedList = mock( LinkedList.class );
 
         //use of the anyInt() argument matcher:
-        when(mockedList.get(anyInt())).thenReturn( "Any int argment matcher used" );
+        when( mockedList.get( anyInt() ) ).thenReturn( "Any int argment matcher used" );
 
         //will return the same thing for any int argument
-        assertEquals(mockedList.get(1),"Any int argment matcher used");
-        assertEquals(mockedList.get(1343434),"Any int argment matcher used");
+        assertEquals( mockedList.get( 1 ), "Any int argment matcher used" );
+        assertEquals( mockedList.get( 1343434 ), "Any int argment matcher used" );
 
         //Use of a custom argument matcher, to get a yes/no decision as to whether or not an argument triggers something:
-        Map mockedMap = mock( HashMap.class);
-        when( mockedMap.get(argThat( new IsStringBeginningWithS() ))).thenReturn( "String beginning with S" );
+        Map mockedMap = mock( HashMap.class );
+        when( mockedMap.get( argThat( new IsStringBeginningWithS() ) ) ).thenReturn( "String beginning with S" );
 
-        assertEquals( "String beginning with S", mockedMap.get("sssss") );      //these arguments match the IsStringBeginningWithS matcher,so mockito returns the designated thing.
-        assertEquals( "String beginning with S", mockedMap.get("Sdfdfdfdfdfd") );
-        assertTrue(mockedMap.get("asas")==null);   //doesn't match the matcher, so returns null.
+        assertEquals( "String beginning with S", mockedMap.get( "sssss" ) );      //these arguments match the IsStringBeginningWithS matcher,so mockito returns the designated thing.
+        assertEquals( "String beginning with S", mockedMap.get( "Sdfdfdfdfdfd" ) );
+        assertTrue( mockedMap.get( "asas" ) == null );   //doesn't match the matcher, so returns null.
 
         //Use of argumentMatcher for multiple parameters, if one param is an ArgumentMatcher, they all have to be.
         AClass mockedClass = mock( AClass.class );
-        when(mockedClass.doSomething( anyString(), anyString(), eq("String 3") )).thenReturn( "hello" );//note the use of the eq(..) argument matcher, that allows for a specific value to be put in.
+        when( mockedClass.doSomething( anyString(), anyString(), eq( "String 3" ) ) ).thenReturn( "hello" );//note the use of the eq(..) argument matcher, that allows for a specific value to be put in.
 
         assertEquals( mockedClass.doSomething( "anyOldthing", "anyOldthing", "String 3" ), "hello" );
         assertNull( mockedClass.doSomething( "anyOldthing", "anyOldthing", "String 4" ) ); //doesn't match the matchers, so returns null.
@@ -126,23 +140,60 @@ public class MockitoTest
 
     }
 
+    /**
+     * U can use mockito to test that some stuff occurred in a certain order.
+     */
+    @Test
+    public void testInvocationOrder()
+    {
+        AClass aClass = mock( AClass.class );
+        InOrder inOrder = inOrder( aClass );
+
+        //call some stuff in an order:
+        aClass.doSomething( "","","" ) ;
+        aClass.doSomethingElse();
+
+        //test will fail here if the order is different to what actually happened:
+        inOrder.verify( aClass ).doSomething( "","","" );
+        inOrder.verify( aClass ).doSomethingElse();
+    }
+
     private class IsStringBeginningWithS extends ArgumentMatcher<String>
     {
         @Override
         public boolean matches( Object argument )
         {
-            return     argument != null
+            return argument != null
                     && argument instanceof String
-                    && (((String)argument).startsWith( "s" ) || ((String)argument).startsWith( "S" ));
+                    && ( ( ( String ) argument ).startsWith( "s" ) || ( ( String ) argument ).startsWith( "S" ) );
         }
     }
 
 
     private class AClass
     {
-        public String doSomething( String s1, String s2, String s3)
+        private AnotherClass anotherClass;
+
+        private void setAnotherClass( AnotherClass anotherClass )
+        {
+            this.anotherClass = anotherClass;
+        }
+
+        public String doSomething( String s1, String s2, String s3 )
         {
             return "aString";
         }
+
+        public void doSomethingElse()
+        {
+
+        }
     }
+
+    private class AnotherClass
+    {
+
+    }
+
+
 }
