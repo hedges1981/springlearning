24a55670acb3300fd5346435ab9ad9d188b8d740
rhall <rhall@sis.tv>
2016-11-17 16:44:25 +0000

    added web socket learning project

--- spring-boot-async-example/complete/src/main/java/hello/Application.java
+++ spring-boot-async-example/complete/src/main/java/hello/Application.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 @EnableAsync
@@ -17,15 +18,14 @@ public class Application extends AsyncConfigurerSupport {
         SpringApplication.run(Application.class, args);
     }
 
-
-
     @Bean
     public Executor asyncExecutor1() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(2);
         executor.setMaxPoolSize(2);
         executor.setQueueCapacity(500);
-
+        //NOTE how we set the rejected execution policy here:
+        executor.setRejectedExecutionHandler( new ThreadPoolExecutor.AbortPolicy() );
         executor.setThreadNamePrefix("asyncExecutor1-");
         executor.initialize();
         return executor;
@@ -34,12 +34,11 @@ public class Application extends AsyncConfigurerSupport {
     @Bean
     public Executor asyncExecutor2() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setRejectedExecutionHandler( new ThreadPoolExecutor.CallerRunsPolicy() );
         executor.setCorePoolSize(2);
         executor.setMaxPoolSize(2);
         executor.setQueueCapacity(500);
-
         executor.setThreadNamePrefix("asyncExecutor2-");
-
         executor.initialize();
         return executor;
     }
